1535b11a0d34a016c89a3845ae17ef6cef9edd3b
joannajjliu <joanna.liu3774@gmail.com>
2020-05-12 20:29:16 -0400

    initial commit

---
+++ smooth-scroll/main.js
@@ -0,0 +1,7 @@
+//Option 2 - jQuery Smooth Scroll (works cross-platform)
+//(didn't work, couldn't find this.hash)
+
+//Option 3- importing and using Smoothscroll CDN:
+var scroll = new SmoothScroll('.navbar a[href*="#"]', {
+    speed: 800
+});
